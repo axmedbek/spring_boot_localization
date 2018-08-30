eef871e4bb967593c2b34b8683eac84dcd94ea94
Ahmad Mammadli <axmed.memmedli.96@mail.ru>
2018-08-30 19:00:06 +0400

    Hibernate Integration was added

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Simple Multilanguage Application with Spring Boot , Thymeleaf
 # Simple Multilanguage Application
  <span style="font-weight:bold">Lang</span> : Java <br>
  <span style="font-weight:bold">Framework</span>  : Spring Boot <br>
+ <span>ORM </span> : Hibernate <br>
  <span style="font-weight:bold">Build Tool</span> : Maven <br>
  <span style="font-weight:bold">Template Engine</span> : Thymeleaf <br>
  <span style="font-weight:bold">Css library</span> : Bootstrap
--- src/main/java/com/axmedbek/controller/api/UserController.java
+++ src/main/java/com/axmedbek/controller/api/UserController.java
@@ -4,10 +4,12 @@ import com.axmedbek.dao.UserDao;
 import com.axmedbek.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +21,11 @@ public class UserController {
     @GetMapping("/users")
     public List<User> allUsers(){
         return userDao.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUser(@PathVariable("id") long id){
+        Optional<User> user = userDao.findById(id);
+        return user;
     }
 }
